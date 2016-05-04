169c4e81469ef3885a56e0d6b698ae2f35fb7d11
hprrkn <prrknh@gmail.com>
2016-05-04 16:23:33 +0900

    make all correspondent to multi-user

--- main.rb
+++ main.rb
@@ -28,7 +28,6 @@ class Tag < ActiveRecord::Base
 end
 
 before do
-  p request.url
   if request.url =~ %r{/login} then
   else
      if !session[:login] then
@@ -36,20 +35,17 @@ before do
        redirect '/login'
      else
        @userId = session[:uId]
+       @userWords = User.find(@userId).words
+       @userTags = User.find(@userId).tags
      end
   end
 end
 
 get '/login' do
-  p "plese login"
-  if @msg.present? then
-    @msg = "plese loginnnn"
-  end
   erb :login
 end
 
 post '/login/check' do
-  p "logincheck"
   @user = User.where({:username => params[:username]}).first
   if @user.present? then
      if @user.password == params[:password] then
@@ -57,58 +53,63 @@ post '/login/check' do
        session[:uId] = @user.id
        redirect '/index'
        erb :index
+     else
+       redirect '/login'
+       erb :login
      end
   else
     redirect '/login'
+    erb :login
   end
 end
 
 
 get '/index' do
-  @countOfMonth = Word.group("strftime('%Y-%m', created_at)").count.each
-  @tags = Tag.all
-  p Tag.all
+  @countOfMonth = @userWords.group("strftime('%Y-%m', created_at)").count.each
+  @tags = @userTags.all
 	erb :index
 end
 
 get %r{/list/(\d{4})\-(\d{2})} do |y,m|
   from = Date::new(y.to_i,m.to_i,1)
   to = from >> 1
-  @words = Word.where("created_at >= ? AND created_at < ?", from.strftime("%Y-%m-%d"), to.strftime("%Y-%m-%d"))
+  @words = @userWords.where("created_at >= ? AND created_at < ?", from.strftime("%Y-%m-%d"), to.strftime("%Y-%m-%d"))
   erb :list
 end  
 
 get %r{/detail/([0-9]*)} do |id| 
-  @word = Word.find(id)
+  @word = @userWords.find(id)
 	erb :detail
 end
 
 get %r{/edit/([0-9]*)} do |id|
-  @word = Word.find(id)
-  @tags = Tag.all
+  @word = @userWords.find(id)
+  @tags = @userTags.all
   erb :edit
 end
 
 get '/tag/list' do
-  @tags = Tag.all
+  @tags = @userTags.all
   erb :tagList
 end
 
 get %r{/tag/detaal/([0-9]*)} do |id|
-  @tag = Tag.find(id)
-  @words = Word.includes(:tags).where('tags.id' => id)
+  @tag = @userTags.find(id)
+  @words = @userWords.includes(:tags).where('tags.id' => id)
   erb :tagDetail
 end
 
 get %r{/tag/ediit/([0-9]*)} do |id|
-  @tag = Tag.find(id)
+  @tag = @userTags.find(id)
   erb :tagEdit
 end
 
 post '/api/new' do
-  new_word = Word.create({:wordtitle => params[:word], :memo => params[:memo]})
-  params['tag_id'].each do |param|
-    new_word.tags << Tag.find(param)
+  new_word = Word.create({:user_id => @userId, :wordtitle => params[:word], :memo => params[:memo]})
+  if params['tag_id'].present? then
+     params['tag_id'].each do |param|
+       new_word.tags << @userTags.find(param)
+     end
   end 
   redirect '/index'
 	erb :index
@@ -116,36 +117,36 @@ end
 
 post '/api/update' do
   wordid = params[:wordid]
-  editword = Word.find(wordid)
+  editword = @userWords.find(wordid)
   editword.update({:wordtitle => params[:word], :memo => params[:memo]})
   editword.tags.clear
   params['tag_id'].each do |param|
-    editword.tags << Tag.find(param)
+    editword.tags << @userTags.find(param)
   end
   redirect "/detail/#{wordid}"
   erb :detail 
 end
 
 post '/api/delete' do 
-  Word.destroy(params['wordid'])
+  @userWords.destroy(params['wordid'])
   redirect '/index'
 	erb :index
 end
 
 post '/api/new/tag' do
-  newTag = Tag.create({:tagname => params[:tagname]})
+  newTag = Tag.create({:user_id => @userId, :tagname => params[:tagname]})
   newTag.to_json(:root => false)
 end
 
 post '/api/tag/delete' do 
-  Tag.find(params['tagid']).destroy
+  @userTags.find(params['tagid']).destroy
   redirect '/tag/list'
   erb :tagList
 end
 
 post '/api/tag/update' do
   tagid = params[:tagid]
-  edittag = Tag.find(tagid)
+  edittag = @userTags.find(tagid)
   edittag.update({:tagname => params[:tagname]})
   redirect "/tag/detaal/#{tagid}"
   erb :tagDetail
